47f909a48498b910b67e8d1db15e6f32e8fa8148
Matthew Mazzuca <matthew.mazzuca@mail.utoronto.ca>
2015-02-25 21:59:27 -0500

    added project files

---
+++ windows.h
@@ -0,0 +1,13 @@
+//
+//  windows.h
+//  neuralnet
+//
+//  Created by Matthew Mazzuca on 2015-02-25.
+//  Copyright (c) 2015 Matthew Mazzuca. All rights reserved.
+//
+
+#ifndef neuralnet_windows_h
+#define neuralnet_windows_h
+
+
+#endif
